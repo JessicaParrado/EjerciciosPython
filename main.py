bc70052251070ea6c65ec4553b4321e1adcc9682
Jessica <jessica.parrado01@correo.usa.edu.co>
2020-09-03 09:28:25 -0500

    Empezando ejercicio

---
+++ main.py
@@ -0,0 +1,5 @@
+print("Binevenido, con este programa podrá concocer su edad en el 2070");
+
+numero = input("Introduzca el año de su nacimiento: ");
+print ("El número que ingresó es:\n",numero);
+print("La edad que usted tendrá en 2070 será de:",2070-numero);
