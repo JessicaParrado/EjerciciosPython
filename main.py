6af147e8f02f22e54d07a79d78e4112bd604dc5a
Jessica <jessica.parrado01@correo.usa.edu.co>
2020-09-03 09:45:38 -0500

    Creando variables y condicionales

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 print("Binevenido, con este programa podrá concocer su edad en el 2070");
 
-numero = input("Introduzca el año de su nacimiento: ");
-print ("El número que ingresó es:\n",numero);
-print("La edad que usted tendrá en 2070 será de:",2070-numero);
+edad=(-1);
+anio=-1;
+while edad<=0:
+    edad = int(input("Introduzca su edad: "));
+    if edad<0:
+        print("Debe introducir un número mayor a cero");
+    else:
+        print("Se ha registrado su edad");
+
+while anio<=0:
+    anio = int(input("Introduzca el año actual: "));
+    if anio<0:
+        print("Debe introducir un año mayor a cero");
+    else:
+        print("Se ha registrado el año");
+
