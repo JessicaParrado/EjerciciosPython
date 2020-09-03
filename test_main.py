663633dfbd5621b55e5438091ed932d0185acaf9
Jessica <jessica.parrado01@correo.usa.edu.co>
2020-09-03 10:20:16 -0500

    Creando prueba unitaria

---
+++ test_main.py
@@ -0,0 +1,22 @@
+from unittest import TestCase
+
+
+class Test(TestCase):
+
+        while edad <= 0 or edad > 2070:
+            edad = int(input("Introduzca su edad: "));
+            if edad < 0:
+                print("Debe introducir un número mayor a cero");
+            else:
+                print("Se ha registrado su edad");
+
+    while anio <= 0:
+        anio = int(input("Introduzca el año actual: "));
+        if anio < 0:
+            print("Debe introducir un año mayor a cero");
+        else:
+            print("Se ha registrado el año");
+
+
+            def test_conocer_edad(self):
+                self.fail()
