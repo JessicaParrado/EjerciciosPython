baae49e56647382ac73fbf78ff788f845e18f883
Jessica <jessica.parrado01@correo.usa.edu.co>
2020-09-03 10:34:35 -0500

    Arreglando prueba

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ print("Binevenido, con este programa podrá concocer su edad en el 2070");
 def conocerEdad(edad,anio):
     numero=0;
     numero=anio-edad;
-    numero=20170-numero;
+    numero=2070-numero;
     print("Su edad para el 2070 será de: ", numero);
+    return numero;
 
 
--- test_main.py
+++ test_main.py
@@ -1,22 +1,22 @@
 from unittest import TestCase
 
-
+from main import conocerEdad
 class Test(TestCase):
 
-        while edad <= 0 or edad > 2070:
-            edad = int(input("Introduzca su edad: "));
-            if edad < 0:
-                print("Debe introducir un número mayor a cero");
-            else:
-                print("Se ha registrado su edad");
+#        while edad <= 0 or edad > 2070:
+ #           edad = int(input("Introduzca su edad: "));
+  #          if edad < 0:
+   #             print("Debe introducir un número mayor a cero");
+    #        else:
+     #           print("Se ha registrado su edad");
 
-    while anio <= 0:
-        anio = int(input("Introduzca el año actual: "));
-        if anio < 0:
-            print("Debe introducir un año mayor a cero");
-        else:
-            print("Se ha registrado el año");
+#    while anio <= 0:
+ #       anio = int(input("Introduzca el año actual: "));
+  #      if anio < 0:
+   #         print("Debe introducir un año mayor a cero");
+    #    else:
+     #       print("Se ha registrado el año");
 
 
-            def test_conocer_edad(self):
-                self.fail()
+ def test_conocer_edad(self):
+   self.assertEqual(69,conocerEdad(19,2020))
