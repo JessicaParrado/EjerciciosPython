7d3f3cfefff9de86f0838c90e01c6cf3cf9ad3b1
Jessica <jessica.parrado01@correo.usa.edu.co>
2020-09-04 13:37:59 -0500

    Ejercicio Terminado con Pruebas

--- test_main.py
+++ test_main.py
@@ -4,7 +4,7 @@ from main import saberPar
 
 class Test(unittest.TestCase):
     def test_saber_Par(self):
-        self.assertEqual(True,saberPar(18))
+        self.assertEqual(True,saberPar(16))
 
 if __name__== '__main__':
     unittest.main()
